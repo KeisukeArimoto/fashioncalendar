b1b88f70fc17bee0560ca467bae9cc8008fb3bec
有本圭佑 <a.ri.777baseball@gmail.com>
2020-12-06 10:35:55 +0000

    log in

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,13 @@
 class User < ApplicationRecord
-    validates :name, presence: true
-    validates :email, presence: true
+ 
+ VALID_EMAIL_REGEX =/\A^([a-zA-Z0-9])+([a-zA-Z0-9\._-])*@([a-zA-Z0-9_-]).([a-zA-Z0-9\._-]+)+\z/i
+  VALID_PASSWORD_REGEX =/\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}\z/i
+
+    validates :name, presence: true, length: { maximum: 15 }  
+    validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
+    validates :password, presence: true, confirmation: true, format: { with: VALID_PASSWORD_REGEX }
+    validates :password_confirmation, presence: true
+    
+    has_secure_password
 
 end
